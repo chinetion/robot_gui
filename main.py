537978027407b7955402fc07e8fb633e366a4204
chinetion <chinetion@gmail.com>
2016-10-17 14:00:55 +0700

    add screen shot

---
+++ main.py
@@ -0,0 +1,15 @@
+from gui import *
+gui = GUI()
+gui.textbox(["Distance",100],["Temparator",200],["Varocity",300])
+gui.textbox(["Distance",100],["Temparator",200],["Varocity",300])
+gui.textbox(["Distance",100],["Temparator",200],["Varocity",300])
+
+gui.label("Distance","Temparator","Distance")
+gui.label("Distance","Temparator","Distance")
+gui.label("Distance","Temparator","Distance")
+
+gui.progressbar("LEFT","RIGHT ")
+
+gui.radio("Manual","Auto")
+
+gui.mainloop()
